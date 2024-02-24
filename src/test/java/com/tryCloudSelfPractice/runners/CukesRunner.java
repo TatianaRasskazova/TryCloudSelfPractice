5ddfe0f329e6c20024b49b4e1fe3447a32f31d6c
TatianaRasskazova <trasskazova840@gmail.com>
2024-02-23 22:46:50 -0600

    Unversioned Files

---
+++ src/test/java/com/tryCloudSelfPractice/runners/CukesRunner.java
@@ -0,0 +1,29 @@
+package com.tryCloudSelfPractice.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = true,
+        tags = "",
+        publish = true // generating a report with public link
+        // monochrome = true --> turning off color, works with pretty plugin above
+
+
+
+
+)
+
+
+public class CukesRunner {}
---
+++ src/test/resources/features/US1.feature
@@ -0,0 +1,2 @@
+Feature: bla
+  Agile story:
